602b9a43e3d2ba52adab6a7de5436ab5433cd1df
Juan Franco <juancaf93@hotmail.com>
2020-08-06 13:45:07 -0500

    Project created

---
+++ Source/NPCxPracticalTest/NPCxPracticalTest.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "NPCxPracticalTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NPCxPracticalTest, "NPCxPracticalTest" );
